60a339db92d70ca1a6124756fc0f5e1201bde881
sushmithachitneni <155569160+sushmithachitneni@users.noreply.github.com>
2024-11-27 11:56:49 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 public class PrimeCheck {
     public static void main(String[] args) {
-        int number = 29; 
+        int number = 9; 
         if (isPrime(number)) {
             System.out.println(number + " is a prime number.");
         } else {
